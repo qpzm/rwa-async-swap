23e0ded2b6829d7db61854ab8292523a7fad11b2
donguks <donguk.seo@modori.io>
2024-12-04 17:18:14 +0900

    Clone uniswapfoundation/scaffold-hook

---
+++ nextjs/components/scaffold-eth/index.tsx
@@ -0,0 +1,11 @@
+export * from "./Address";
+export * from "./Balance";
+export * from "./BlockieAvatar";
+export * from "./Contract";
+export * from "./Faucet";
+export * from "./FaucetButton";
+export * from "./Input";
+export * from "./RainbowKitCustomConnectButton";
+
+export const TOKEN0_CONTRACT_ADDRESS = "0x615bCf3371F7daF8E8f7d26db10e12F0F4830C94";
+export const TOKEN1_CONTRACT_ADDRESS = "0x3D5e538D212b05bc4b3F70520189AA3dEA588B1E";
