3f74dfa259c8a7e28f87e145ecd5fd882a7a05ad
donguks <donguk.seo@modori.io>
2024-12-06 13:04:52 +0900

    FIx address

--- nextjs/wagmi.config.ts
+++ nextjs/wagmi.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
           31337: "0x22a8e2faa3ce538630b8867c587a044a9a8cc888",
         },
         PoolManager: {
-          31337: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+          31337: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
         },
         PoolSwapTest: {
           31337: "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
